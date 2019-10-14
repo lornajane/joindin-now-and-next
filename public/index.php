9f669e81501fcb939b91fcb26c0818d54eba0d56
Alfie Foster <bonzi@lordbonzi.pro>
2019-10-14 10:39:13 +0000

    Add 5 Minuite Browser Refresh via Meta Tags

--- public/index.php
+++ public/index.php
@@ -94,6 +94,8 @@ function getTalk($talk) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+<meta http-equiv="refresh" content="300"/>
 </head>
 <body>
 <div class="navBar">
